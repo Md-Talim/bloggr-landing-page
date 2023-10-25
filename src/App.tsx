f2688ecbca83baaa224eec418ab2be350da99928
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-25 11:08:59 +0530

    Initial commit
    Set up the project with tailwindcss and add assets

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="bg-lightRed">Blogr Landing Page</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        // Primary
+        lightRed: 'hsl(356, 100%, 66%)', // CTA text
+        veryLightRed: 'hsl(355, 100%, 74%)', // CTA hover background
+        veryDarkBlue: 'hsl(208, 49%, 24%)', // headings
+
+        // Neutral
+        white: 'hsl(0, 0%, 100%)', // text
+        grayishBlue: 'hsl(240, 2%, 79%)', // footer text
+        veryDarkGrayishBlue: 'hsl(207, 13%, 34%)', // body copy
+        veryDarkBlackBlue: 'hsl(240, 10%, 16%)', // footer background
+      },
+    },
+  },
+  plugins: [],
+};
