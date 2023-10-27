5aa0d04e99f42bef287d0a10817c5f7e4f12e621
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-27 12:23:42 +0530

    Build the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Button from './components/Button';
 import Navbar from './components/Navbar';
 
 const App = () => {
@@ -7,6 +8,22 @@ const App = () => {
       <header className="container md:px-5">
         <Navbar />
       </header>
+
+      <section className="container mt-28 md:mt-20">
+        <div className="text-center">
+          <h1 className="text-4xl md:text-7xl text-white font-bold">
+            A modern publishing platform
+          </h1>
+          <p className="text-lg md:text-xl mt-5 text-white">
+            Grow your audience and build your online brand
+          </p>
+        </div>
+
+        <div className="flex gap-6 mt-10 text-center w-full items-center justify-center">
+          <Button label="Start for Free" variant="primary" />
+          <Button label="Learn More" variant="underlined" />
+        </div>
+      </section>
     </main>
   );
 };
