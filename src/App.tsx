277c30462aa61e39db25c6ef57362967857a255e
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-30 18:20:31 +0530

    Build the features section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { features } from './data';
 import Hero from './sections/Hero';
 import Overview from './sections/Overview';
+
+const Description = ({
+  label,
+  description,
+}: {
+  label: string;
+  description: string;
+}) => (
+  <div className="space-y-4">
+    <h3 className="text-3xl font-semibold text-veryDarkBlue">{label}</h3>
+    <p className="text-veryDarkGrayishBlue max-w-[55ch] leading-relaxed md:text-lg">
+      {description}
+    </p>
+  </div>
+);
 
 const App = () => {
   return (
@@ -26,6 +42,27 @@ const App = () => {
               your site competitive.
             </p>
           </div>
+        </div>
+      </section>
+
+      <section className="mt-20 md:mt-60 xl:mr-40 flex flex-col xl:flex-row max-sm:gap-10 items-center xl:justify-between">
+        <div className="xl:-translate-x-[23%] xl:scale-125">
+          <picture>
+            <source
+              media="(min-width:768px)"
+              srcSet="/images/illustration-laptop-desktop.svg"
+            />
+            <img
+              src="/images/illustration-laptop-mobile.svg"
+              alt="Laptop Illustration"
+            />
+          </picture>
+        </div>
+
+        <div className="space-y-8 max-sm:text-center px-8">
+          {features.map((feature) => (
+            <Description {...feature} />
+          ))}
         </div>
       </section>
     </>
--- src/data.ts
+++ src/data.ts
@@ -25,3 +25,16 @@ export const info = [
       "Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections. format. or flow. With this functionality.  you're in full control.",
   },
 ];
+
+export const features = [
+  {
+    label: 'Free, open, simple',
+    description:
+      'Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools. and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.',
+  },
+  {
+    label: 'Powerful tooling',
+    description:
+      'Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.',
+  },
+];
